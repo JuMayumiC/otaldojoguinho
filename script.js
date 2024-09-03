4f54fd6ffe6fc13b292be4b2d96a5e3d9329f1f1
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-09-03 08:35:44 -0300

    hg

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function mostraAlternativa (){
 
 function respostaSelecionada (pergunta){
     const afirmações = pergunta.afirmação;
-    historiafinal +=  afirmações + ", , ".split("\n");
+    historiafinal +=  afirmações + " ";
     atual++;
     mostraPergunta ();
 }
